297db788b88b47ff8fc348ba871e13ec0c89fa2d
BryanKongXJ <bryankong17@gmail.com>
2022-03-11 12:47:56 +0800

    git commit src/components/Button.vue

---
+++ src/components/Button.vue
@@ -0,0 +1,38 @@
+<template>
+    <button :class="[greenButtonTheme ? 'greenbg' : 'whitebg', 'baseButton']" @click="buttonClicked"> {{ buttonText }} </button>
+</template>
+ 
+<script>
+export default {
+    props: {
+        buttonText : String,
+        greenButtonTheme: Boolean,
+    },
+
+    methods: {
+        buttonClicked() {
+            this.$emit("bc");
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .baseButton {
+        padding: 20px 120px;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+
+    .whitebg {
+        background-color: white;
+        color: green;
+    }
+
+    .greenbg {
+        background-color: green;
+        color: white;
+    }
+
+</style>
