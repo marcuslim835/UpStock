5f6d9949323d37cdaf3d3627bb2b9b7f3cd6e95c
marcuslim835 <77620616+marcuslim835@users.noreply.github.com>
2022-03-17 11:16:07 +0800

    Updated News Search and Display
    1. News articles are all rendered in cards now
    2. Added CSS for all cards
    3. Added v-ifs for item fields that could potentially be blank
    4. Changed display of item.description to v-html since it could contain html elements

--- src/components/News_Article.vue
+++ src/components/News_Article.vue
@@ -1,45 +1,65 @@
 <template>
 
-  <div class="main">
-    <CCard style="width: 18rem" v-for="(item, index) in articles" :key="index">
-      <CCardImage orientation="top" v-bind:src="item.urlToImage" alt="logo" />
+  <div class="main" id="help">
+    <CRow :xs="{ cols: 1, gutter: 4 }" :md="{ cols: 3}">
+    <CCard class="card" style="width: 18rem" v-for="(item, index) in articles" :key="index" >
+      <CCardImage orientation="top" v-bind:src="item.urlToImage" alt="News Article Image" />
       <CCardBody>
         <CCardTitle>{{ item.title }}</CCardTitle>
-        <CCardSubtitle> By {{ item.author }} at {{ item.source.name }}</CCardSubtitle>
+        <CCardSubtitle> 
+          <span v-if="item.author"> By {{ item.author }} </span> 
+          <span v-if="item.source.name"> From {{ item.source.name }} </span>
+        </CCardSubtitle>
         <CCardText>
-            {{ item.description }}
+          <span v-html="item.description"></span>
         </CCardText>
-        <CCardLink v-bind:href="item.url"> Read more here </CCardLink >
+        <CButton component="a" v-bind:href="item.url" variant="outline"> Read more here </CButton>
       </CCardBody>
       <CCardFooter> {{ item.publishedAt }} </CCardFooter>
     </CCard>
+    </CRow>
   </div>
 </template>
 
 <script>
-import { CCard, CCardBody, CCardTitle, CCardText, CCardLink, CCardImage, CCardFooter, CCardSubtitle } from "@coreui/vue";
+import { CRow, CCard, CCardBody, CCardTitle, CCardText, CButton, CCardImage, CCardFooter, CCardSubtitle } from "@coreui/vue";
+
 
 export default {
   components: {
+    CRow,
     CCard,
     CCardBody,
     CCardTitle,
     CCardText,
-    CCardLink,
+    CButton,
     CCardImage,
     CCardFooter,
     CCardSubtitle
   },
   props: ["articles"],
   mounted() {
-      
-      console.log("MOUNTING",this.articles);
+      console.log("MOUNTING in News_Article: ",this.articles);
   },
 };
 </script>
-
 <style scoped>
-CCardImage {
-
+div.card {
+  background-color:rgba(255, 255, 255, 0.05);
+  color:#868E96;
 }
+h5.card-title {
+  color:#F8F9FA;
+}
+a.btn {
+  background-color: #F8F9FA;
+  color: #868E96;
+}
+a.btn:hover {
+  background-color: #F8F9FA;
+  color: green;
+}
+</style>
+
+<style scoped src='@coreui/coreui/dist/css/coreui.min.css'>
 </style>
