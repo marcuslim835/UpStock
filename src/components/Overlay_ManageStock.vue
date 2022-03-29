70259ebc746735ff91f742e71f0e03a6ffc29116
BryanKongXJ <bryankong17@gmail.com>
2022-03-29 12:30:54 +0800

    Added new tag options, resolved functionality for adding stock into firebase

--- src/components/Overlay_ManageStock.vue
+++ src/components/Overlay_ManageStock.vue
@@ -10,23 +10,26 @@
             <select id="brokers"> 
                 <option hidden> Select Broker: </option>
                 <option value="DBS Vickers"> DBS Vickers </option>
-                <option value="MooMoo"> MooMoo </option>
+                <option value="Moo Moo"> MooMoo </option>
                 <option value="Tiger Brokers"> Tiger Brokers </option>
                 <option value="UOB Kay Hian"> UOB Kay Hian </option>
+                <option value="UOB Kay Hian"> Others </option>
                 <!-- Add brokers we have here -->
             </select><br><br>
 
             <input type="text" class="textbox" id="stockname" placeholder="Stock Name"><br><br>
+            <input type="text" class="textbox" id="ticker" placeholder="Ticker (e.g GOOGL)"><br><br>
             <input type="text" class="textbox" id="quantity" placeholder="Quantity"><br><br>
             <input type="text" class="textbox" id="price" placeholder="Price"><br><br>
-            <input type="text" class="textbox" id="purchasedate" placeholder="Date of Purchase"><br><br> 
+            <input type="date" class="textbox" id="purchasedate" placeholder="Date of Purchase"><br><br> 
             
             <select id="tags"> 
-                <option value="finance"> Finance </option>
-                <option value="reit"> REIT </option>
-                <option value="tech"> Tech </option>
-                <option value="others"> OTHERS </option>
-
+                <option value="Tech"> Tech </option>
+                <option value="Reit"> REIT </option>
+                <option value="Finance"> Finance </option>
+                <option value="Energy"> Energy </option>
+                <option value="Industrials"> Industrials </option>
+                <option value="Others"> OTHERS </option>
                 <!-- Can add more tags here -->
             </select><br><br>
 
@@ -39,12 +42,12 @@
 
 <script>
 import Button from "./Button.vue" 
-import firebaseApp from "../api/firebaseAccessor.js"; 
-import {getFirestore } from "firebase/firestore";
-import {doc, setDoc} from "firebase/firestore";
+//import firebaseApp from "../api/firebaseAccessor.js"; 
+//import {getFirestore } from "firebase/firestore";
+//import {doc, setDoc} from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from "firebase/auth"
-//import * as ST from '../api/holdingsAccess.js';
-const db = getFirestore(firebaseApp);
+import * as ST from '../api/holdingsAccess.js';
+//const db = getFirestore(firebaseApp);
 
 export default {
     //props: ["toggleModal"],
@@ -81,30 +84,39 @@ export default {
 
     methods: {
         async saveToFirebase() {
+            
             let stockName = document.getElementById("stockname").value;
+            let ticker = document.getElementById("ticker").value;
             let qty = document.getElementById("quantity").value;
             let price = document.getElementById("price").value;
-            let purchaseDate = document.getElementById("purchasedate").value;
-            let brokerName = document.getElementById("broker").value;
-
-            alert("Saving ...")
-
-            // NEED FIREBASE FIRESTORE INFORMATION...
+            //let purchaseDate = document.getElementById("purchasedate").value;
+            let brokerName = document.getElementById("brokers").value;
+            let tag = document.getElementById("tags").value;
+            console.log(tag)
 
             try {
+                
                 const auth = getAuth();
                 const currUser = auth.currentUser;
-                let userPortfolio = doc(db, currUser.uid, "Portfolio", stockName)
-                const docRef = await setDoc(userPortfolio, {Stock : stockName, Broker : brokerName, Buy_Price : price, Quantity : qty, Purchase_Date : purchaseDate})
+                
+                ST.addData(currUser.uid, ticker, stockName, brokerName, price, qty, tag)
+                console.log("AFTER ADD DATA")
+
+                /*
+                let userPortfolio = doc(db, String(currUser.uid), "holdings")
+                const docRef = await setDoc(userPortfolio, {[ticker]: {broker: {[brokerName]: {qty: qty, price: price}}, name: stockName, type: tag}})
+                const docRef = await setDoc(doc(db, String(currUser.uid), "holdings"), {GOOGL: {broker: {UOBKayHian: {qty: 100, price: 90}}, name: "Google", type: "Tech"}})     // HARD CODE TEST
                 console.log(docRef)
                 document.getElementById("userForm").reset();
                 this.$emit("added")
+                ST.addData(currUser, stockName, brokerName, price, qty, purchaseDate, tag)
+                */
             } 
             catch(error) {
                 console.error("Error adding document: ", error);
             }
 
-            this.$router.push({name : 'Portfolio'});
+            //this.$router.push({name : 'Portfolio'});
         },
 
         toggleModal() {
